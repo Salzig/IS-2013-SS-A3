f790aa1a5e882099c3940c2aa34e126941730f43
Ben Rexin <salzig+github@gmail.com>
2013-05-14 00:24:30 +0200

    refactor

--- board.js
+++ board.js
@@ -1,30 +1,42 @@
 var field = generate_field();
 
-var board = d3.select('body').append("ul").classed('towers',1);
+var game = (function() {
+  var board = d3.select('body').append("ul").classed('towers',1);
+  return {
+    draw: function(matrix) {
+      var towers = board.selectAll('li')
+        .data(matrix, function(data, index) { return index })
 
-var towers = board
-  .selectAll('li')
+      towers
+        .enter()
+          .append("li")
+          .attr("tower", function(d,i) { return i })
+      towers
+        .exit().remove();
 
+      var stones = towers.selectAll("button")
+        .data(function(matrix) { return matrix })
 
-var stones = towers
-  .data(field)
-  .enter()
-    .append("li")
-    .attr("tower", function(d,i) { return i })
-    .selectAll("button")
-    .data(function(d) { return d; }, function(d,i) { return i; })
+      stones
+        .enter()
+          .append("button")
+          .attr("class", function(d) { return d > 0 ? "white" : "black" })
+          .attr("stone", function(d,i) { return i })
+          .on("click", function(d, i) {
+            var x = parseInt(this.parentElement.attributes.tower.value);
+            var y = i;
+            console.log("Tower " + x, "Stone " + y)
 
-stones
-  .enter()
-    .append("button")
-    .attr("class", function(d) { return d > 0 ? "white" : "black" })
-    .attr("stone", function(d,i) { return i })
-    .on("click", function(d, i) {
-      var x = parseInt(this.parentElement.attributes.tower.value);
-      var y = i;
-      console.log("Tower " + x, "Stone " + y)
+            field[x] = field[x].slice(0,y);
+            game.draw(field)
+          })
+      stones
+        .exit().remove()
 
-      field[x] = field[x].slice(0,y);
-    })
+      console.log(board);
+      console.log(towers);
+    }
+  }
+})();
 
-stones.exit().remove();
+game.draw(field);
